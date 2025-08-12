9165258824706a617b721822d32b268ffe10f3dd
KavinduAmalka <146797841+KavinduAmalka@users.noreply.github.com>
2025-08-12 18:45:03 +0530

    implement user registration

--- backend/confligs/db.js
+++ backend/confligs/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
      mongoose.connection.on("connected", () => console.log("Database connected")
     );
-    await mongoose.connect(`${process.env.MONGODB_URI}/The Bottle House`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/TheBottleHouse`)
   } catch (error) {
     console.error(error.message);
   }
