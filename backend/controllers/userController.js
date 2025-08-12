e4e2c756a865a5949b679b10fa29cfbe44249523
KavinduAmalka <146797841+KavinduAmalka@users.noreply.github.com>
2025-08-12 19:36:08 +0530

    implement user login API

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,13 +8,13 @@ export const register = async (req, res)=> {
     const {name, email, password} = req.body;
 
     if(!name || !email || !password){
-      return res.json({success: false, message: "Please fill all the fields"});
+      return res.status(400).json({success: false, message: "Please fill all the fields"});
     }
 
     const existingUser = await User.findOne({email})
 
     if(existingUser){
-      return res.json({success: false, message: "User already exists"});
+      return res.status(400).json({success: false, message: "User already exists"});
     }
 
     const hashedPassword = await bcrypt.hash(password, 10)
@@ -31,6 +31,42 @@ export const register = async (req, res)=> {
     })
 
     return res.json({success: true, user: {email: user.email, name: user.name}})
+
+  }catch(error){
+    console.error(error.message);
+    res.json({success: false, message: error.message});
+  }
+}
+
+// Login a user: /api/user/login
+
+export const login = async (req, res) => {
+  try{
+     const {email, password} = req.body;
+
+     if(!email || !password)
+      return res.status(400).json({success: false, message: "Email and password are required"});
+     const user = await User.findOne({email});
+
+     if(!user){
+      return res.status(401).json({success: false, message: "Invalid email or password"});
+     }
+
+     const isMatch = await bcrypt.compare(password, user.password)
+
+      if(!isMatch)
+        return res.status(401).json({success: false, message: "Invalid email or password"});
+      
+       const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn:'7d'});
+
+      res.cookie('token',token, {
+        httpOnly: true, 
+        secure: process.env.NODE_ENV === 'production', 
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+        maxAge: 7 * 24 * 60 * 60 * 1000 
+      })
+
+      return res.json({success: true, user: {email: user.email, name: user.name}})
 
   }catch(error){
     console.error(error.message);
