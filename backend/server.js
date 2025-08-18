f820d0f38fc8b022691c350a929f5e6e9c69c84b
KavinduAmalka <146797841+KavinduAmalka@users.noreply.github.com>
2025-08-19 02:57:25 +0530

    added vercel frontend link in to the allowedOrigins

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ await connectDB()
 
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://liquar-shop.vercel.app']
 
 app.post('/stripe',express.raw({type: 'application/json'}), stripeWebhooks)
 
