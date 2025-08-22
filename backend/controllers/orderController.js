6f92bcbf3824cdad528f814472b6d86087431bd9
KavinduAmalka <146797841+KavinduAmalka@users.noreply.github.com>
2025-08-22 08:50:16 +0530

    change currenccy

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -72,7 +72,7 @@ export const placeOrderStripe = async (req, res) => {
       const line_items = productData.map((item)=>{
         return {
           price_data: {
-            currency: 'usd',
+            currency: 'LKR',
             product_data: {
               name: item.name,
             },
